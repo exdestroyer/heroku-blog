84dd5a8f844213dbaee67c2e6c36a192f61415ba
EXDESTROYER\Helvin <453027255@qq.com>
2016-11-26 17:06:51 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
 // webpack.config.js
 var webpack = require('webpack')
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+// multiple extract instances
+let cssExtractor = new ExtractTextPlugin('stylesheets/[name].css');
+let lessExtractor = new ExtractTextPlugin('stylesheets/[name].less');
 
-if(process.env.NODE_ENV === 'development'){
-  var loaders = ['react-hot','babel']
+if (process.env.NODE_ENV === 'development') {
+  var loaders = ['react-hot', 'babel']
 } else {
   var loaders = ['babel']
 }
@@ -15,10 +19,23 @@ module.exports = {
     publicPath: '/dist/'
   },
   module: {
-    loaders: [{
+    loaders: [
+    {
       test: /\.js$/,
       loaders: loaders,
       exclude: /node_modules/
+    }, 
+    {
+      test: /\.css$/,
+      loader: "style!css"
+    }, 
+    {
+      test: /\.(jpg|png)$/,
+      loader: "url?limit=8192"
+    }, 
+    {
+      test: /\.scss$/,
+      loader: cssExtractor.extract(['css','sass'])
     }]
   },
   plugins: [
@@ -26,6 +43,7 @@ module.exports = {
       'process.env.COSMIC_BUCKET': JSON.stringify(process.env.COSMIC_BUCKET),
       'process.env.COSMIC_READ_KEY': JSON.stringify(process.env.COSMIC_READ_KEY),
       'process.env.COSMIC_WRITE_KEY': JSON.stringify(process.env.COSMIC_WRITE_KEY)
-    })
- ]
+    }),
+    cssExtractor
+  ]
 };
